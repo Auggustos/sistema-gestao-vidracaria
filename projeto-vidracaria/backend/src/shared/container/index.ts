ca527bc72aee22af4379f12306ffc9b7c879f6b7
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2021-07-30 19:56:31 -0300

    chore: register singleton to inject dependency

--- projeto-vidracaria/backend/src/shared/container/index.ts
+++ projeto-vidracaria/backend/src/shared/container/index.ts
@@ -2,9 +2,12 @@ import { container } from 'tsyringe';
 
 import IProductsRepository from '@modules/products/repositories/IProductsRepository';
 import ICustomersRepository from '@modules/customers/repositories/ICustomersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 import ProductsRepository from '@modules/products/infra/typeorm/repositories/ProductsRepository';
 import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
 
 // <IProductsRepository> garante que o tipo de ProductsRepository seja do IProductsRepository
 
@@ -16,4 +19,9 @@ container.registerSingleton<IProductsRepository>(
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
   CustomersRepository
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository
 );
