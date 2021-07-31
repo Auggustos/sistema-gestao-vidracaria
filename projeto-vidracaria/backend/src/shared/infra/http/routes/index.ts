5bc28578ed4a8bef4beb7d1d9bdc4cec0a9db68f
Thiago Siqueira <thiagosiqueira@unifei.edu.br>
2021-07-30 23:03:32 -0300

    chore: creation routes

--- projeto-vidracaria/backend/src/shared/infra/http/routes/index.ts
+++ projeto-vidracaria/backend/src/shared/infra/http/routes/index.ts
@@ -4,11 +4,13 @@ import productsRouter from '@modules/products/infra/http/routes/products.routes'
 import customersRouter from '@modules/customers/infra/http/routes/customers.routes';
 import swaggerDocs from '@shared/infra/http/routes/swagger.json';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
 const routes = Router();
 
 routes.use('/products', productsRouter);
-routes.use('/sessions', sessionsRouter);
+routes.use('/auth', sessionsRouter);
+routes.use('/users', usersRouter);
 routes.use('/customers', customersRouter);
 routes.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
